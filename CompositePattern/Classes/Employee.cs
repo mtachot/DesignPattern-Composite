9a6f913642f812a404b6511286881d278220ff19
mtachot <matthieu.tachot@hotmail.fr>
2017-07-13 10:01:45 +0200

    First commit

---
+++ CompositePattern/Classes/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern.Classes
+{
+    public class Employee
+    {
+        private string Name;
+        private string Department;
+        private int Salary;
+        private List<Employee> Subordinates;
+
+        public Employee(string name, string department, int salary)
+        {
+            this.Name = name;
+            this.Department = department;
+            this.Salary = salary;
+            Subordinates = new List<Employee>();
+        }
+
+        public void add(Employee e)
+        {
+            Subordinates.Add(e);
+        }
+
+        public void remove(Employee e)
+        {
+            Subordinates.Remove(e);
+        }
+
+        public List<Employee> getSubordinates()
+        {
+            return Subordinates;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Employee :[ Name: {0}, Department: {1}, Salary: {2} ]", Name, Department, Salary);
+        }
+    }
+}
---
+++ CompositePattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using CompositePattern.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee CEO = new Employee("John", "CEO", 1000000);
+            Employee headSales = new Employee("Sarah", "Head Sales", 200000);
+            Employee headMarketing = new Employee("Carlos", "Head Marketing", 200000);
+
+            Employee clerk1 = new Employee("Laura", "Marketing", 60000);
+            Employee clerk2 = new Employee("Bob", "Marketing", 60000);
+
+            Employee salesExecutive1 = new Employee("Richard", "Sales", 55000);
+            Employee salesExecutive2 = new Employee("Elizabeth", "Sales", 55000);
+
+            CEO.add(headMarketing);
+            CEO.add(headSales);
+
+            headMarketing.add(clerk1);
+            headMarketing.add(clerk2);
+
+            headSales.add(salesExecutive1);
+            headSales.add(salesExecutive2);
+
+            Console.WriteLine(CEO.ToString());
+
+            foreach (Employee headEmployee in CEO.getSubordinates())
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(headEmployee.ToString());
+                foreach (Employee employee in headEmployee.getSubordinates())
+                {
+                    Console.WriteLine(employee.ToString());
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
